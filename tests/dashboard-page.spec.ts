1f5233178d60a64af1e46cc107c475c890362375
sumana <sumanarai110@gmail.com>
2026-05-27 16:53:13 -0400

    yml file change

--- tests/dashboard-page.spec.ts
+++ tests/dashboard-page.spec.ts
@@ -3,15 +3,12 @@ import { test, expect } from '../fixtures/baseTest'
 const productName = 'ZARA COAT 3';
 
 
-
-
 test('Add to cart button is visible', async ({ loginPage, dashboardPage }) => {
 
     await expect(dashboardPage.getAddToCartButton(productName)).toBeVisible();
 });
 
 test('search product and add to cart', async ({ loginPage, dashboardPage }) => {
-
 
     await dashboardPage.searchProductAndAddToCart(productName);
     await dashboardPage.navigateToCart();
